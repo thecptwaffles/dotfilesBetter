46914b3112564e11be13da0710d6b520d98adb6c
thecptwaffles <65668198+thecptwaffles@users.noreply.github.com>
2023-06-29 10:57:17 -0600

    nix configs

---
+++ flatpakInstall.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+echo installing flatpaks
+
+flatpak install flathub com.spotify.Client
+
+flatpak install flathub com.discordapp.Discord
+
+flatpak install flathub org.prismlauncher.PrismLauncher
+
+flatpak install flathub com.heroicgameslauncher.hgl
+
+flatpak install flathub net.lutris.Lutris
